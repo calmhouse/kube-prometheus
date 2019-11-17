da5e203ac96ace6170a9dd25f39f22dc9efb171d
calmhouse <calmhouse@users.noreply.github.com>
2019-11-16 22:11:41 -0500

    Add install/uninstall scripts

---
+++ install.sh
@@ -0,0 +1,5 @@
+set -e 
+
+kubectl create -f manifests/setup
+until kubectl get servicemonitors --all-namespaces ; do date; sleep 1; echo ""; done
+kubectl create -f manifests/
---
+++ uninstall.sh
@@ -0,0 +1,3 @@
+set -e
+
+kubectl delete --ignore-not-found=true -f manifests/ -f manifests/setup
